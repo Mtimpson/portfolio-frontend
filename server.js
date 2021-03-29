b27dd2ad573479ecead0800b20f6a8a8b98ff9e1
Mtimpson <mdtimpson@gmail.com>
2021-03-28 21:04:41 -0400

    update server.js to fix build

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const app = express();
 app.use(requireHTTPS);
-app.use(express.static('./dist/<name-on-package.json>'));
+app.use(express.static('./dist/portfolio-frontend'));
 
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/<name-on-package.json>/' }
+    res.sendFile('index.html', { root: 'dist/portfolio-frontend/' }
     );
 });
 
