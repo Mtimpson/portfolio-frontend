b7b6f082b3cb9bb6099e44ee98347bd1f1559d63
Mtimpson <mdtimpson@gmail.com>
2021-10-07 23:28:20 -0400

    minor clean up of updated home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   title = 'angularTests';
-  list = ['Software Engineer.', 'Builder.', 'Creator.', 'Digital Architect.', 'Computer Scientist.',
-    'Full Stack Developer.', 'Lover of Tech.', 'Problem Solver.', 'Leader', 'Out of the Box Thinker.', 'Communicator']
+  list = ['software engineer.', 'builder of the web.', 'creator.', 'digital architect.', 'computer scientist.',
+    'full stack developer.', 'lover of tech.', 'problem solver.', 'leader.', 'out of the box thinker.',
+    'coffee drinker.', 'mobile developer.']
 
   constructor() { }
 
